a437896cc3e0a0f20869e138955dca1811b95a7c
Mahfuz_Alam_Rafi <mahfuzrafu@gmail.com>
2025-01-02 14:33:52 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ connectdb(); // Connect to the MongoDB database
 // middleware
 const cors = require('cors');
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL
+  origin: 'https://uber-clone-gyzy.vercel.app', // Replace with your frontend URL
   credentials: true, // Allow credentials
 }));
 
